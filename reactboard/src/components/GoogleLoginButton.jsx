11dadd8a846547073b9ed50490521521837403ab
ehdrjs4502 <ehdrjs4502@naver.com>
2024-01-16 17:19:40 +0900

    구글 로그인 access token 받도록 수정

--- reactboard/src/components/GoogleLoginButton.jsx
+++ reactboard/src/components/GoogleLoginButton.jsx
@@ -1,26 +1,21 @@
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode';
+import { useGoogleLogin } from '@react-oauth/google';
 
 const GoogleLoginButton = () => {
-  const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+  // 구글 로그인 버튼 함수
+  const onClickLoginBtn = useGoogleLogin({
+    onSuccess: tokenResponse => console.log(tokenResponse.access_token),
+  });
 
   /*
-    credential에 있는 정보 백엔드한테 보내서 신규 회원인지 아닌지 확인
+    access 토큰 백엔드한테 보내서 신규 회원인지 아닌지 확인
     신규 회원이면 닉네임, 성별 지정하는 창으로 이동
     기존 회원이면 로그인 성공하고 백엔드한테 받은 토큰 로컬스토리지에 저장 후 메인페이지로 이동
   */
   return (
     <div>
-      <GoogleOAuthProvider clientId={clientId}>
-        <GoogleLogin
-          onSuccess={credentialResponse => {
-            console.log(jwtDecode(credentialResponse.credential));
-          }}
-          onFailure={err => {
-            console.log(err);
-          }}
-        />
-      </GoogleOAuthProvider>
+      <button type="button" onClick={() => onClickLoginBtn()}>
+        <span>구글로 로그인 하기</span>
+      </button>
     </div>
   );
 };
