d9b6a26a9d410d09efa3ff07bcd41153e019ebd2
ehdrjs4502 <ehdrjs4502@naver.com>
2024-01-23 17:12:37 +0900

    댓글 작성 구현

--- reactboard/src/components/comment/WriteComment.jsx
+++ reactboard/src/components/comment/WriteComment.jsx
@@ -1,11 +1,26 @@
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
+import useInput from '../../hooks/useInput';
 
 const WriteComment = () => {
+  const [value, setValue, handleChange] = useInput('');
+
+  const handleWriteCommentButton = () => {
+    setValue('');
+    // 댓글 api 구현하기
+  };
+
   return (
     <div>
-      <TextField id="input-with-icon-textfield" variant="standard" />
-      <Button variant="outlined">댓글 작성</Button>
+      <TextField
+        id="input-with-icon-textfield"
+        variant="standard"
+        value={value}
+        onChange={handleChange}
+      />
+      <Button variant="outlined" onClick={handleWriteCommentButton}>
+        댓글 작성
+      </Button>
     </div>
   );
 };
