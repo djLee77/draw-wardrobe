66fd9b655e0d1195325f822a6fa3d3b8f84efe46
ehdrjs4502 <ehdrjs4502@naver.com>
2024-02-19 14:50:16 +0900

    글 내용 수정

--- reactboard/src/components/Post/Content.jsx
+++ reactboard/src/components/Post/Content.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'; // prop-types 불러오기
 const Content = ({ content }) => {
   return (
     <div>
-      <p>{content}</p>
+      <p>자유롭게 옷을 움직여보세요!</p>
+      <div>{content}</div>
     </div>
   );
 };
