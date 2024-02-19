1fe03345bdd3fe77d0fa56acc59900842c05e385
ehdrjs4502 <ehdrjs4502@naver.com>
2024-02-19 15:42:27 +0900

    댓글 기능 구현

--- reactboard/src/components/comment/CommentList.jsx
+++ reactboard/src/components/comment/CommentList.jsx
@@ -1,22 +1,27 @@
+import { useEffect, useState } from 'react';
 import Comment from './Comment';
 import WriteComment from './WriteComment';
 
-const comments = [
+const testComments = [
   {
     id: 1,
     nickname: 'dd',
     comment: '와우~',
-    date: '2024-01-20',
+    date: '2024.1.20 15:30',
   },
 ];
 
 // 댓글 목록 불러오기 api 작성
 
 const CommentList = () => {
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    setComments(testComments);
+  }, []);
   return (
     <div>
       <h4>댓글 목록</h4>
-      <WriteComment />
+      <WriteComment comments={comments} setComments={setComments} />
       {comments.map(comment => (
         <Comment key={comment.id} comment={comment} />
       ))}
--- reactboard/src/components/comment/WriteComment.jsx
+++ reactboard/src/components/comment/WriteComment.jsx
@@ -2,12 +2,23 @@ import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import useInput from '../../hooks/useInput';
 
-const WriteComment = () => {
+const WriteComment = ({ comments, setComments }) => {
   const [value, setValue, handleChange] = useInput('');
 
   const handleWriteCommentButton = () => {
+    if (value === '') return;
     setValue('');
     // 댓글 api 구현하기
+    const newComment = {
+      id: comments.length + 1, // 기존 댓글 개수 + 1로 id 생성 (일반적으로 서버에서 관리하는 값이어야 하지만 예시에서는 간단하게 구현)
+      nickname: '사용자 닉네임', // 사용자의 닉네임 또는 사용자 정보에서 가져온 값으로 설정
+      comment: value, // 입력한 댓글 내용
+      date: `${new Date().toISOString().slice(0, 10).replace(/-/g, '.')} 
+        ${new Date().toLocaleTimeString()}`, // 현재 날짜와 시간을 이용하여 YYYY.MM.DD HH:mm 형식으로 변환
+    };
+
+    // 새로운 댓글을 기존 댓글 목록에 추가하고 상태 업데이트
+    setComments([...comments, newComment]);
   };
 
   return (
