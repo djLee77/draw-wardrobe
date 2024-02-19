e0b652c762cef4f2bb9fbe548ce5cc3009f8aabb
ehdrjs4502 <ehdrjs4502@naver.com>
2024-02-19 15:52:11 +0900

    댓글 삭제 구현

--- reactboard/src/components/comment/Comment.jsx
+++ reactboard/src/components/comment/Comment.jsx
@@ -1,11 +1,17 @@
 import PropTypes from 'prop-types';
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, onDeleteComment }) => {
+  const onClickDelBtn = () => {
+    onDeleteComment(comment.id);
+  };
   return (
     <div>
       <h4>{comment.nickname}</h4>
       <span>{comment.comment}</span>
       <p>{comment.date}</p>
+      <button type="button" onClick={onClickDelBtn}>
+        🚮
+      </button>
       <hr />
     </div>
   );
--- reactboard/src/components/comment/CommentList.jsx
+++ reactboard/src/components/comment/CommentList.jsx
@@ -18,12 +18,27 @@ const CommentList = () => {
   useEffect(() => {
     setComments(testComments);
   }, []);
+
+  // 댓글 삭제 함수 (댓글 삭제 api로 수정해야함)
+  const onDeleteComment = commentId => {
+    // filter 함수를 사용하여 해당 ID를 가진 댓글을 제외한 새로운 배열 생성
+    const updatedComments = comments.filter(
+      comment => comment.id !== commentId,
+    );
+    // 새로운 배열로 댓글 목록 상태 업데이트
+    setComments(updatedComments);
+  };
+
   return (
     <div>
       <h4>댓글 목록</h4>
       <WriteComment comments={comments} setComments={setComments} />
       {comments.map(comment => (
-        <Comment key={comment.id} comment={comment} />
+        <Comment
+          key={comment.id}
+          comment={comment}
+          onDeleteComment={onDeleteComment}
+        />
       ))}
     </div>
   );
